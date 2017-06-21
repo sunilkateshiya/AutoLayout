58d70d523b2b1b772ef5f37c049c4e816c0740a3
ChavdaKishan <chavdakish1996@gmail.com>
2017-06-21 11:57:01 +0530

    ok

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var a = 10 , b = 20 , c = a+b
+        
+        print(c)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+   
 }
 
+class Vehicle {
+    var currentSpeed = 0.0
+    var description: String {
+        return "traveling at \(currentSpeed) miles per hour"
+    }
+    func makeNoise() {
+        // do nothing - an arbitrary vehicle doesn't necessarily make a noise
+    }
+}
+
+
+
+
+
+//let names = ["Anna", "Alex", "Brian", "Jack"]
+//
+////for name in names 
+//    {
+//    print("Hello, \(name)!")
+//}
+//// Hello, Anna!
+//// Hello, Alex!
+//// Hello, Brian!
+//// Hello, Jack!
